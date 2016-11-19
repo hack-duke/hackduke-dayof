9ecd08329e03645e4f651f9845e3c43426b371c6
David Yang <dy46@duke.edu>
2016-11-19 01:40:12 -0500

    fixed speakers and datasets

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -21,7 +21,7 @@ let mentorshipRightText = ['are students who work with Community Empowerment Fun
 let sponsorshipLeftTitles = ['Using the Co-Lab\'s APIs to access Duke Data', 'How to use ArcGIS JavaScript API with ArcGIS Online to visualize data', 'React Messaging App Workshop']
 let sponsorshipLeftTimes = ['Saturday Nov 19, 3:00 pm', 'Saturday Nov 19, 4:15pm', 'Saturday Nov 19, 5:00 pm']
 let sponsorshipLeftLocations = ['Hudson Hall 207', 'Hudson 125', 'Hudson HALL 207']
-let sponsorshipLeftSpeakers = ['SALLY HALL: ', 'NATHAN WU: ', 'JIM POSEN: ']
+let sponsorshipLeftSpeakers = ['SALLY HALL: ', 'NATHAN WU: ', 'MICHAEL DENG: ']
 let sponsorshipLeftText = ['Attendees will learn what API endpoints the Co-Lab has, what data can be accessed using them, and how to integrate them into applications, including authentication requirements',
   'This workshop is providing a lighting way of converting any csv data with geocode or physical address into a featureLayer in ArcGIS Online, and how to use ArcGIS JavaScript to renderer those string data and project on the map.',
   'A project driven workshop where we use ReactJS to make a basic Messaging App.']
@@ -29,7 +29,7 @@ let sponsorshipLeftText = ['Attendees will learn what API endpoints the Co-Lab h
 let sponsorshipRightTitles = ['Writing server-less apps on the Ethereum blockchain', 'Blockchain, ConsenSys, Ethereum, and INFURA']
 let sponsorshipRightTimes = ['Saturday Nov 19, 5:00 pm', 'Saturday Nov 19, 8:30 pm']
 let sponsorshipRightLocations = ['Hudson Hall 208', 'Hudson Hall 207']
-let sponsorshipRightSpeakers = ['JIM POSEN:', 'BRENT XU and MICHAEL WUEHLER']
+let sponsorshipRightSpeakers = ['JIM POSEN: ', 'BRENT XU and MICHAEL WUEHLER']
 let sponsorshipRightText = ['An introduction to blockchain technologies, covering what they are and the powerful paradigm shift they have enabled. We will focus on the Ethereum protocol and show how you can get started writing applications using it without any backend servers.',
   'will be discussing ConsenSys and the role that it plays in the BlockChain community.  Additionally we will go into details of developer tools that have been created within ConsenSys including INFURA and other various Ethereum related Dapps.  The talk will focus on innovations in the Blockchain space as well as opportunities and advancements in this fast growing industry.']
 
